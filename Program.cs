b2cd38edeb4a219ed88bb4fdb5d2f4d81863cdbc
5Andrei <120333207+5Andrei@users.noreply.github.com>
2024-02-05 12:07:47 +0300

    Creating GetArrLessOrEqualThreeSymbols

--- Program.cs
+++ Program.cs
@@ -3,3 +3,24 @@
     Console.Write(msg);
     return Console.ReadLine().Split("; ");
 }
+string[] GetArrLessOrEqualThreeSymbols(string[] arr)
+{
+    int count = 0;
+    string[] fresh = new string[count];
+    string[] main = fresh;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3) 
+        {
+            count++;
+            fresh = new string[count];
+            for (int j = 0; j < fresh.Length - 1; j++) 
+            {
+                fresh[j] = main[j];
+            }
+            fresh[count - 1] = arr[i];
+            main = fresh;
+        }
+    }
+    return main;
+}
